c3b18ee4dfa1afd140b81ee63c6065a4045e9423
Bismark Agyare <bismarkagyare54@gmail.com>
2023-03-28 16:57:41 +0000

    refactor createNavbar function

--- src/index.js
+++ src/index.js
@@ -1,46 +1,32 @@
-function createHeader () {
+function createNavItem (text, url) {
+  const navItem = document.createElement('li');
+  const navLink = document.createElement('a');
+  navLink.textContent = text;
+  navLink.href = url;
+  navLink.classList.add('nav-link');
+  navItem.appendChild(navLink);
+  return navItem;
+}
+
+function createNavbar() {
   const nav = document.createElement('nav');
   nav.classList.add('nav');
 
   const restaurantName = document.createElement('div');
   restaurantName.textContent = 'Riola';
   restaurantName.classList.add('restaurant-name');
+  nav.appendChild(restaurantName);
 
   const navLinks = document.createElement('ul');
   navLinks.classList.add('nav-links');
-
-  const home = document.createElement('li');
-  const homeLink = document.createElement('a');
-  homeLink.textContent = 'home';
-  homeLink.href = '#';
-  home.appendChild(homeLink);
-
-  const menu = document.createElement('li');
-  const menuLink = document.createElement('a');
-  menuLink.textContent = 'menu';
-  menuLink.href = '#';
-  menu.appendChild(menuLink);
-
-  const contact = document.createElement('li');
-  const contactLink = document.createElement('a');
-  contactLink.textContent = 'contact';
-  contactLink.href = '#';
-  contact.appendChild(contactLink);
-
-  const className = 'nav-link';
-  [home, menu, contact].forEach(navlink => {
-    navlink.classList.add(className);
-  });
-
-  navLinks.appendChild(home);
-  navLinks.appendChild(menu);
-  navLinks.appendChild(contact);
-
-  nav.appendChild(restaurantName);
+  navLinks.appendChild(createNavItem('home', '#'));
+  navLinks.appendChild(createNavItem('menu', '#'));
+  navLinks.appendChild(createNavItem('contact', '#'));
   nav.appendChild(navLinks);
 
   const content = document.getElementById('content');
   content.appendChild(nav);
 }
 
-createHeader();
+createNavbar();
+
