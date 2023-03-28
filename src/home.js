0ad9a2b8bc8f538bc895a132eb67c4d228bf06d1
Bismark Agyare <bismarkagyare54@gmail.com>
2023-03-28 18:16:20 +0000

    add createHome function

---
+++ src/home.js
@@ -0,0 +1,29 @@
+function createHome () {
+  const homeContainer = document.createElement('div');
+  homeContainer.classList.add('home-container');
+
+  const homeHeading = document.createElement('h3');
+  homeHeading.classList.add('home-heading');
+  homeHeading.textContent = 'Come enjoy some delicious marocain cuisines';
+
+  const homeParagraph = document.createElement('p');
+  homeParagraph.classList.add('home-paragraph');
+  homeParagraph.textContent = 'Check out our menu below';
+
+  const homeImage = document.createElement('img');
+  homeImage.src = 'images/menu-profile.jpg';
+  homeImage.alt = 'home-img';
+
+  const homeButton = document.createElement('button');
+  homeButton.classList.add('home-button');
+  homeButton.textContent = 'order now';
+
+  homeContainer.appendChild(homeHeading);
+  homeContainer.appendChild(homeParagraph);
+  homeContainer.appendChild(homeImage);
+  homeContainer.appendChild(homeButton);
+
+  return homeContainer;
+}
+
+export default createHome;
