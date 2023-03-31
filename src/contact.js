edc0bdbb0b80be39effee654314358ec185822c6
Bismark Agyare <bismarkagyare54@gmail.com>
2023-03-31 12:13:39 +0000

    add create contact functionality

---
+++ src/contact.js
@@ -0,0 +1,18 @@
+function createContact() {
+  const contactContainer = document.createElement('div');
+  contactContainer.classList.add('contact-container');
+
+  const locationContainer = document.createElement('div');
+  locationContainer.classList.add('location-container');
+  const locationIcon = document.createElement('i');
+  locationIcon.classList.add('fa fap-map-marker');
+  const locationText = document.createElement('p');
+  locationText.textContent = '1040 Gare de Train, Settat';
+
+  const scheduleContainer = document.createElement('div');
+  scheduleContainer.classList.add('schedule-container');
+  const scheduleIcon = document.createElement('i');
+  scheduleIcon.classList.add('fa fa-calender');
+  const scheduleText = document.createElement('p');
+  scheduleText.textContent = 'Monday-Friday: 8h-23h';
+}
